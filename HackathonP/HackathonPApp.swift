dbc88bab5a47328ad089493cc6034b66fe27add0
Greenpiligrim <greenpiligrim@icloud.com>
2023-11-24 23:32:24 +0100

    Initial Commit

---
+++ HackathonP/HackathonPApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonPApp.swift
+//  HackathonP
+//
+//  Created by Greenpilligrim on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HackathonPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
